561de26e061f07e37e0767d00b4da0bdbc4131ea
Luca Billat <lucabillatpro@gmail.com>
2024-11-05 15:08:00 +0100

    add dist folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base:  "https://luca-b431.github.io/ohmyfood/",
   build: {
     rollupOptions: {
       input: {
